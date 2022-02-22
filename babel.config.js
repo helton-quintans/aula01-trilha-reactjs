0b623fff77b45c8f3700ddee61fda9158a6f646c
Helton Quintãns <helton.quit@gmail.com>
2022-02-21 23:58:10 -0300

    webpack dev server

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 
 module.exports = {
-    presets: [
-        '@babel/preset-env',
-        '@babel/preset-react',
-    ]
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', {
+      runtime: 'automatic'
+    }],
+  ]
 
 }
