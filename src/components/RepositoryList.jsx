5e33835921588df5359c1156d882ba9cde3b5602
Helton Quintãns <helton.quit@gmail.com>
2022-02-22 16:15:36 -0300

    using map to list the repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,14 +2,6 @@ import React, { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 
-//https://api.github.com/users/helton-quintans
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform',
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([])
 
@@ -24,10 +16,9 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
